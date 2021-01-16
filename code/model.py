da33559225e33cbfacdcd700eff6a240b91c8511
RenzeD <guitarenze@hotmail.nl>
2021-01-16 01:18:53 +0100

    jfesklfjsdklfs

--- code/model.py
+++ code/model.py
@@ -1,18 +1,19 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib
-
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+# from moviepy.video.io.bindings import mplfig_to_npimage
+# import moviepy.editor as mpy
 from matplotlib import animation
 
 
 def default_coupling(n):
-    return 0.2 * np.ones((n, n))
+    return 0.3 * np.ones((n, n))
 
 
 def default_noise(n):
-    return 1.2 * np.random.normal(0, 1, n)
+    return 0.1 * np.random.normal(0, 1, n)
 
 
 class Kuramoto:
@@ -57,21 +58,29 @@ class Kuramoto:
         return solution
 
     @staticmethod
-    def draw(solution, timespan):
+    def draw(solution, timespan, filename=None):
         fig, ax = plt.subplots()
         size = len(solution[0])
         x, y = np.cos(solution[:, 0]), np.sin(solution[:, 0])
 
         mat, = ax.plot(x, y, 'o')
 
-        def animate(i):
+        def animate_matplotlib(j):
+            i = int(j*size/(60*timespan))
             x, y = np.cos(solution[:, i]), np.sin(solution[:, i])
             mat.set_data(x, y)
             ax.set_title('t=' + str(i * timespan / size))
             return mat,
 
+        # def animate_moviepy(j):
+        #     i = int(j*size/timespan)
+        #     x, y = np.cos(solution[:, i]), np.sin(solution[:, i])
+        #     mat.set_data(x, y)
+        #     ax.set_title('t=' + str(i * timespan / size))
+        #     return mplfig_to_npimage(fig)
+
         ax.axis([-1.5, 1.5, -1.5, 1.5])
-        anim = animation.FuncAnimation(fig, animate, frames=size, interval=timespan * (10 ** 3) / size)
+        anim = animation.FuncAnimation(fig, animate_matplotlib, frames=int(60*timespan), interval=10**3/60, blit=False)
         return anim
 
 
@@ -90,11 +99,15 @@ if __name__ == '__main__':
     sim_params = {
         'timespan': [0, 20],
         'method': 'RK45',
-        'rtol': 1e-3,
-        'atol': 1e-6,
-        'max step': 20 / 10000
+        'rtol': 1e90,
+        'atol': 1e90,
+        'max step': 20 / 2400
     }
 
     result = model.run(sim_params)
-    anim = model.draw(result.y, sim_params['timespan'][1])
-    # anim.save('k0.1r0.1')
+    movie = model.draw(result.y, sim_params['timespan'][1])
+
+    plt.rcParams['animation.ffmpeg_path'] = r"C:\Users\guita\PycharmProjects\BEP\code\bin\ffmpeg.exe"
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=60, bitrate=8192, codec='libx265')
+    movie.save('k0.3r0.1.mp4', writer=writer)
